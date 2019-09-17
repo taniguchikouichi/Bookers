970f67bf48d0fff40b4bb4bae173859635fa26b6
谷口晃一 <taniguchikouichi@taniguchikouichinoMacBook-Pro.local>
2019-09-17 18:24:09 +0900

    [Finsh]4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+  def index
+      @book = Book.new
+      @books = Book.all
+  end
+
+  def show
+      @book = Book.find(params[:id])
+  end
+  def new
+  end
+
+  def edit
+      @book = Book.find(params[:id])
+  end
+
+  def update
+      @book = Book.find(params[:id])
+         if @book.update(book_params)
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render("books/edit")
+  end
+end
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render("books/index")
+
+    end
+  end
+
+  def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
+
